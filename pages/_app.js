bae85d60d78d3f4c5fedc2f57f43e25da560db0e
gfgullo <gfgullo@gmail.com>
2021-05-02 19:58:05 +0200

    added tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-KDTX8HW'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
