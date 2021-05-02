b3f9e18d034d8f943f3a1944eb4e983d9ca0bf8d
gfgullo <gfgullo@gmail.com>
2021-05-02 20:45:51 +0200

    function fix #3

--- api/contact.js
+++ api/contact.js
@@ -1,12 +1,9 @@
 const admin = require("firebase-admin");
-import serviceAccount from './gameletics-eaa39-firebase-adminsdk-9hmeh-5c4f72356d.json';
 
 module.exports = (req, res) => {
 
   const email = req.body.email
   const language = req.body.language
-
-  functions.logger.info(req.body, { structuredData: true });
 
   admin.initializeApp({
     credential: admin.credential.cert({
