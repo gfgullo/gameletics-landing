9fce94c13dc192d410388278f5653ef31032af3d
gfgullo <gfgullo@gmail.com>
2021-05-03 11:29:12 +0200

    function fix #4

--- api/contact.js
+++ api/contact.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
   const email = req.body.email
   const language = req.body.language
 
+  try {
   admin.initializeApp({
     credential: admin.credential.cert({
       project_id: "gameletics-eaa39",
@@ -12,6 +13,13 @@ module.exports = (req, res) => {
       client_email: "firebase-adminsdk-9hmeh@gameletics-eaa39.iam.gserviceaccount.com"
     }),
   });
+}
+catch (error){
+  if (!/already exists/u.test(error.message)) {
+    // eslint-disable-next-line no-console
+    console.error('Firebase admin initialization error', error.stack);
+  }
+}
 
   const db = admin.firestore();
   const docRef = db.collection("gameletics").doc(email);
